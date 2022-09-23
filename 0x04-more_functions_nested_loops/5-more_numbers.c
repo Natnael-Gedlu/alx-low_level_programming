4b84eba35ab2c473cc1ae16eceea8434c54c0764
Natnael-Gedlu <nategd800@gmail.com>
2022-09-23 05:01:02 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,19 +6,16 @@
  * Return: Always 0
  */
 
-void more_numbers(void);
+void more_numbers(void)
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			{
 				_putchar('0' + (j / 10));
-			}
 			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
