c31cffb667035be3fe87d71a8e6392954b52acb8
Natnael-Gedlu <nategd800@gmail.com>
2022-09-23 04:22:08 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * print_numbers - prints numbers 0-9 except 2&4
+ * print_most_numbers - prints numbers 0-9 except 2&4
  *
  * Return: Always 0
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i = 0;
 
