d144b866d0d29858707593d98238d52f440435be
Natnael-Gedlu <nategd800@gmail.com>
2022-09-28 14:56:17 -0700

    4-pow_recursion.c and 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+include "main.h"
+
+/**
+ * power_operation - returns the natural square root of a number
+ * @n: number
+ * @m: square root of number
+ * Return: (m) else (-1)
+ */
+int power_operation(int n, int m)
+{
+	if (m % (n / m) == 0)
+	{
+		if (m * (n / m) == n)
+			return (m);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, m + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * Return: square root of (n)
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
+}
