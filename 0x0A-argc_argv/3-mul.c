d9f59ac48d72d6994fb38c853b51309282aef69b
Natnael-Gedlu <nategd800@gmail.com>
2022-09-30 13:49:32 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,12 +13,12 @@ int main(int argc, char *argv[])
 {
 int x, y;
 if (argc == 3)
-	{
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-	printf("%d\n", x * y);
-	return (0);
-	}
+{
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+printf("%d\n", x *y);
+return (0);
+}
 print("Error\n");
 return (1);
 }
