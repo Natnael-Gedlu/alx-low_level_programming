8033f2b22f0672a2946bc45ecd902ac0facfb246
Natnael-Gedlu <nategd800@gmail.com>
2022-10-07 14:18:22 -0700

    code corrections for 1-string_nconcat and also added 3-array_range.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,10 +15,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len = n, index;
 
 	if (s1 == NULL)
-		s1 = "",
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 = "",
+		s2 = "";
 
 	for (index = 0; s1[index]; index++)
 		len++;
@@ -29,6 +29,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
 
 	for (index = 0; s2[index] && index < n; index++)
 		concat[len++] = s2[index];
