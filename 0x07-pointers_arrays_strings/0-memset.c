cfa968098de52bbe739aa39bb9b1cc2a388302a2
Natnael-Gedlu <nategd800@gmail.com>
2022-10-08 04:56:03 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memory area
+ * @s: pointer to the block of memory to fill
+ * @b: value to be set
+ * @n: bytes to be set to value
+ * Return: a pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int index;
+	unsigned char *memory = s, value = b;
+
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+
+	return (memory);
+}
