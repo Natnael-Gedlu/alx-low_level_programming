59c5862a1280ca22497071a9ab9f77866ed4d008
Natnael-Gedlu <nategd800@gmail.com>
2022-10-12 03:08:02 -0700

    1-array_iterator.c & 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * int_index - searches for an integer.
+ * @size: number of elements in the array.
+ * @cmp: pointer to the function.
+ * @array: array to be searched.
+ * Return: index of the first element, if no element
+ * matches return -1.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (array && cmp)
+	{
+		for (x = 0; x < size; x++)
+		{
+		if (cmp(array[x]) != 0)
+			return (x);
+		}
+	}
+	return (-1);
+}
