0ad74bcab1e23ebd474d70447055d5f015cda0dd
Natnael-Gedlu <nategd800@gmail.com>
2022-10-13 06:30:34 -0700

    0-sum_them_all.c & 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <variadic_functions.h>
+
+/**
+ * print_numbers - function prints numbers.
+ * @n: number of functions.
+ * separator: string to be printed.
+ * @...: variable numbers to be printed.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list m;
+	unsigned int index;
+
+	va_start(m, n);
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(m, int));
+
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(m);
+}
