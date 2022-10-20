60307aa04d082b79354769471e03a53c812c158c
Natnael-Gedlu <nategd800@gmail.com>
2022-10-20 13:10:10 -0700

    mandatory tasks

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+SECTION .print
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .print
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
